57ac5c93d845f7baf4dcccb8c313fab8e2e304f4
Salomao Rodrigues <salomao@Salomaos-MacBook-Pro.local>
2015-10-22 15:44:37 +0100

    Mapp Show model fields to feed

--- app/model/Show.js
+++ app/model/Show.js
@@ -1,19 +1,31 @@
 Ext.define('TvShows.model.Show', {
     extend: 'Ext.data.Model',
-    
+
+    requires: [
+        'Ext.data.field.Date',
+    ],
+
     fields: [
-        { name: 'first_aired', type: 'date' },
-        { name: 'title', type: 'string' },
-        { name: 'episode', type: 'auto' },
-        { name: 'genre', type: 'auto' },
-        { name: 'overview', type: 'string' },
-        { name: 'network', type: 'string' },
-        { name: 'runtime', type: 'int' },
-        { name: 'logo', type: 'string' },
-        { name: 'thumb', type: 'string' },
-        { name: 'fanart', type: 'string' },
-        { name: 'year', type: 'int' },
-        { name: 'slug', type: 'string' }
+        {
+            type: 'date',
+            name: 'first_aired',
+            dateFormat: 'C',
+            convert: function(data) {
+                return data;
+            },
+            mapping: 'show.first_aired'
+        },
+        { name: 'title', type: 'string', mapping: 'show.title' },
+        { name: 'episode', type: 'auto', mapping: 'show.year' },
+        { name: 'genre', type: 'auto', mapping: 'show.genre' },
+        { name: 'overview', type: 'string', mapping: 'show.overview' },
+        { name: 'network', type: 'string', mapping: 'show.network' },
+        { name: 'runtime', type: 'int', mapping: 'show.runtime' },
+        { name: 'logo', type: 'string', mapping: 'show.images.logo.full' },
+        { name: 'thumb', type: 'string', mapping: 'show.images.poster.thumb' },
+        { name: 'fanart', type: 'string', mapping: 'show.images.fanart.full' },
+        { name: 'year', type: 'int', mapping: 'show.year' },
+        { name: 'slug', type: 'string', mapping: 'show.ids.slug' }
 
     ]
 });
